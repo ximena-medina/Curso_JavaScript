1c812d61fd5873de4c8d614adb8a9d5960b4c763
ximena <xime.mediin@gmail.com>
2025-02-20 12:56:01 -0600

    New feature :sparkles: Mostrando los diferentes tipos de loops, su funcion y estructura

---
+++ Loop/for.js
@@ -0,0 +1,13 @@
+// Estructura de for
+
+// for (variable; condicion; incremento) {
+//     codigo a ejecutar
+// }  
+            //0     //1      //2     //3
+let list = ["eat", "sleep", "code", "repeat"];
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+}
+
+
+//mientras la variable sea igual a 0 y 0 sea menor que la longitud de la lista (4) incrementa 1
---
+++ Loop/while.js
@@ -0,0 +1,14 @@
+/* 
+
+while(condicion) { si la condicion sea verdad se ejecuta el codigo de adentro
+    codigo
+}
+*/
+
+let contador = 0 // inicia en 0
+
+while(contador < 11) {
+    console.log(contador) 
+    contador++ //va a empezar a incrementar
+} //se rompe cuando el contador es igual a 10
+
