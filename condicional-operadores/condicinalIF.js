aa20d5b91f98f1b2f039f071c4552f36c04a3679
ximena <xime.mediin@gmail.com>
2025-02-24 10:02:57 -0600

    New fwature :sparkles: agregar condicionales y operadores basicos de JavaScript

---
+++ condicional-operadores/condicinalIF.js
@@ -0,0 +1,10 @@
+let nombre = "emilio"
+if (nombre === "emiliano") {
+    console.log("hola emiliano")
+
+} else if (nombre === "emilio"){
+    console.log("hola emilio")
+
+} else {
+    console.log("nombre no encontrado")
+}
---
+++ condicional-operadores/operadores.js
@@ -0,0 +1,18 @@
+// operadores de comparacion 
+
+""=="" //Compara dos valores para ver si son iguales, pero sin tener en cuenta el tipo de datos. 
+""==="" //Compara dos valores para ver si son iguales y del MISMO TIPO DE DATOS.
+""!=""//Compara dos valores para ver si son DIFERENTES
+""!==""//Compara dos valores para ver si son diferentes o de diferentes tipos de datos.
+"">""//Un valor es mayor
+""<""//Un valor menor
+"">=""//Mayor o igual
+""<=""//Menor o igual
+
+const a = 10
+const b = 20
+const c = "10"
+
+console.log(a == b)
+console.log(!(a === b)) //Adentro decimos que a y b no son iguales sin embargo si son diferentes y es true la diferencia.
+
